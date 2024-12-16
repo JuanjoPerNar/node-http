c895d0c55316c7acca7120271ada7a0e92ca8722
Guanjino <jjpereiranaranjo@gmail.com>
2024-12-16 18:48:09 +0100

    Challenge terminado

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = {
+    title: 'Servidor HTTP',
+    subtitle: 'Ejercicio servidor HTTP con plantilla HTML',
+    description: 'El objetivo de este ejercicio es crear un servidor HTTP en Node.js que responda con un documento HTML que contiene información específica de una página web. La información debe provenir de un objeto almacenado en otro módulo.'
+};
+
+module.exports = data;
---
+++ server.js
@@ -0,0 +1,25 @@
+const data = require('./data.js');
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+    res.end(`
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>${data.title}</title>
+        </head>
+        <body>
+            <h1>${data.title}</h1>
+            <h2>${data.subtitle}</h2>
+            <p>${data.description}</p>       
+        </body>
+        </html>
+        `);
+});
+
+server.listen(0, () => {
+    console.log(`Server listening on port http://localhost:${server.address().port}`);    
+});
+
